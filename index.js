41ee4b8b635948a4eec909d8a982175a30eea4a5
apivattua <101862813+apivattua@users.noreply.github.com>
2022-10-12 14:05:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send("Hello!!")
+})
+
+app.get('/test',(req,res) => {
+    res.send("testing..!!")
+})
+
+app.listen(3000, ()=>{
+    console.log('server is running..')
+})
+
